1205bc31139f5db23aff78ac0bcd87cb2efd0b5d
alexm <alexm@ALEX-DEV>
2023-09-21 18:04:53 -0300

    Add project files.

---
+++ src/Trabalhador/Trabalhador.cs
@@ -0,0 +1,26 @@
+﻿namespace Trabalhador;
+
+public static class Trabalhador
+{
+	private static int TentativaAtual = 0;
+
+	public static void TrabalhoDemorado()
+	{
+		Thread.Sleep(TimeSpan.FromMinutes(1));
+	}
+
+	public static void TrabalhoComErro()
+	{
+		throw new Exception("Este é o método com erro!");
+	}
+
+	public static void TrabalhoComDificuldades()
+	{
+		TentativaAtual++;
+
+		if (TentativaAtual < 5 || TentativaAtual % 2 == 0)
+			throw new Exception($"Esta tentativa ocasionou um erro. Tentativa número {TentativaAtual}");
+
+		Console.WriteLine("Método sucesso.");
+	}
+}
