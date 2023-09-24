9df84d32e9a32ad46436f7e74cdbc43f8b20d9b8
Alex Mariano <alexmarianokotcz@outlook.com.br>
2023-09-24 09:38:54 -0300

    adicionando o pacote e configurando

---
+++ src/ClienteServico/HangfireConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+
+namespace Demonstracao.API;
+
+public static class HangfireConfiguration
+{
+	public static IServiceCollection AddHangfireService(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddHangfire(configuration => configuration
+	   .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+	   .UseSimpleAssemblyNameTypeSerializer()
+	   .UseRecommendedSerializerSettings()
+	   .UsePostgreSqlStorage(config.GetConnectionString("HangfireDatabase")));
+
+		services.AddHangfireServer(options =>
+		{
+			options.Queues = new[]
+			{
+				"filademorada",
+				"filadeerros",
+				"filacomdificuldades"
+			};
+		});
+
+		return services;
+	}
+
+	public static IApplicationBuilder AddHangfireServer(this IApplicationBuilder app)
+	{
+		app.UseHangfireDashboard();
+
+		return app;
+	}
+}
--- src/ClienteServico/Program.cs
+++ src/ClienteServico/Program.cs
@@ -1,3 +1,5 @@
+using Demonstracao.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHangfireService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.AddHangfireServer();
 
 app.Run();
